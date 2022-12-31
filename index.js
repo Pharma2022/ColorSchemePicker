fbdb01a7b7e5703b5e37e9ea8d3c8042a5f5280d
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-12-31 08:50:04 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const hexObjectArray= [
     
 ]
 
-document.getElementById("grid").addEventListener("click",async e=>await navigator.clipboard.writeText(e.target.dataset.color) )
-
+document.getElementById("grid").addEventListener("click", e=>navigator.clipboard.writeText(e.target.dataset.color) )
 document.getElementById("get").addEventListener("click",e=>  {
     e.preventDefault()
     let myColor= document.getElementById("myColor").value 
@@ -34,7 +33,7 @@ const setHtml= data=>{
      document.getElementById("grid").innerHTML= 
      data.map(item => 
      `<div class="gridItem" style="background-color:${item.hex.value}" data-color="${item.hex.value}"> </div>
-    <div class="colorName" "data-color="${item.hex.value}">${item.hex.value}</div> `).join(" ") +` <h4 id="copy">Click on palette or hex value to copy!</h4>`
+    <div class="colorName" "data-color="${item.hex.value}">${item.hex.value}</div> `).join(" ") 
 
 }
 
